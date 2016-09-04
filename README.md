bc09f72fad364155d96464240ac90d3a5653f836
flynn <niceilm@naver.com>
2016-09-04 11:51:23 +0900

    init files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Type definitions for meteor-migrations 0.9.8
+- Project https://github.com/percolatestudio/meteor-migrations
+- Definitions by flynn <https://github.com/niceilm/>
+
+# Example
+```
+import { Migrations } from 'meteor/percolate:migrations';
+```
---
+++ migrations.d.ts
@@ -0,0 +1,33 @@
+/**
+ * @author https://github.com/niceilm/
+ * @link https://github.com/percolatestudio/meteor-migrations
+ * @description Type definitions for meteor-migrations 0.9.8
+ * @example
+ * import { Migrations } from 'meteor/percolate:migrations';
+ * Migrations.migrateTo('3,rerun');
+ */
+declare module 'meteor/percolate:migrations' {
+  interface MigrationConfig {
+    // Log job run details to console
+    log: boolean;
+    // Use a custom logger function (defaults to Meteor's logging package)
+    logger: any;
+    // Enable/disable logging "Not migrating, already at version {number}"
+    logIfLatest: boolean;
+    // migrations collection name to use in the database
+    collectionName: string;
+  }
+  interface MigrationAction {
+    version: number;
+    name: string;
+    up: Function;
+    down?: Function;
+  }
+  interface MigrationsStatic {
+    migrateTo(target: string): void;
+    add(option: MigrationAction): void;
+    config(option: MigrationConfig): void;
+    start(): void;
+  }
+  export let Migrations: MigrationsStatic;
+}
